406b0430e0543d35b32eb9b4d4494c7566fcbc97
Harry Westbrook <breakitup@hotmail.co.uk>
2025-02-02 15:07:39 +0000

    Prompt for names action

--- actions/index.js
+++ actions/index.js
@@ -6,6 +6,7 @@ const StartTimer = require('./startTimer')
 const StopTimer = require('./stopTimer')
 const TogglePresenterAccess = require('./togglePresenterAccess')
 const ToggleIndividualPresenterAccess = require('./toggleIndividualPresenterAccess')
+const PromptForNames = require('./promptForNames');
 
 module.exports = function (self) {
 	self.setActionDefinitions({
@@ -16,6 +17,7 @@ module.exports = function (self) {
 		startTimer: StartTimer(self),
 		stopTimer: StopTimer(self),
 		togglePresenterAccess: TogglePresenterAccess(self),
-		toggleIndividualPresenterAccess: ToggleIndividualPresenterAccess(self)
+		toggleIndividualPresenterAccess: ToggleIndividualPresenterAccess(self),
+		promptForNames: PromptForNames(self),
 	})
 }
---
+++ actions/promptForNames.js
@@ -0,0 +1,13 @@
+module.exports = function (self) {
+	return {
+		name: 'Prompt For Names',
+		description: 'Use this if you enable presenter access control after presenters have joined and have not had to enter their names',
+		callback: (event) => {
+			if (self.connection) {
+				self.connection
+					.invoke('promptUnnamedUsers', self.config.code)
+					.catch((err) => self.log('error', err.toString()))
+			}
+		},
+	}
+}
